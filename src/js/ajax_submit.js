d1aa0cbf2ec446a8f643e21792680b6e8674345a
Bilal YILMAZ <mail@bilalyilmaz.com>
2020-07-14 11:39:30 +0300

    ajax-submit event oluşturuldu

--- src/js/ajax_submit.js
+++ src/js/ajax_submit.js
@@ -8,74 +8,85 @@ import 'jquery-form';
 			$(this).ajaxForm({
 				dataType: 'json',
 				beforeSubmit: () => { $(this).find('button').prop("disabled", 1); },
-				success:ajaxFormSuccess,
+				success:function (response, statusText, xhr, $form) {
+					$(".form-control").removeClass("is-invalid is-valid");
+					$form.find(".invalid-feedback").remove();
+					$form.find('button').prop("disabled", 0);
+					if (response.situ === 'success')
+					{
+
+						$form.before(
+							'<div class="alert alert-success" role="alert">'+
+							'<h4 class="alert-heading">'+(response.message !== undefined && response.message.title !== undefined?response.message.title:'İşlem Tamam')+'</h4>'+
+							'<p>'+(response.message !== undefined && response.message.content !== undefined ? response.message.content:'İşleminiz başarıyla gerçekleştirildi')+'</p>'+
+							'</div>'
+						);
+						if(response.removeForm === false)
+						{
+							$(".alert").slideUp(1000);
+						}
+						else
+						{
+							$form.remove();
+						}
+
+						if(response.hideModal)
+						{
+							if (typeof response.hideModal === 'boolean')
+								$(".modal").modal("hide");
+							else if (typeof response.hideModal === 'string')
+								$(response.hideModal).modal("hide");
+						}
+
+						if (response.alert !== undefined && typeof window.success == "function" )
+						{
+							window.success(response.alert);
+						}
+					}
+					else
+					{
+						if (response.alert !== undefined && typeof window.error == "function" )
+						{
+							window.error(response.alert);
+						}
+
+						$form.find(".form-control").each(function () {
+							$(this).addClass("is-valid");
+						});
+						$.each(response.errors, function(name, err) {
+							let errorsText = '<ul class="invalid-feedback list-unstyled">';
+							$.each(err, function(n, errorText) {
+								errorsText += "<li>"+errorText+"</li>";
+							});
+							errorsText += "</ul>";
+							$("#"+name)
+								.removeClass("is-valid")
+								.addClass("is-invalid")
+								.after(errorsText);
+						});
+
+					}
+					if (response.refresh) window.location.reload();
+					else if (response.url) { window.location.href = response.url; }
+					else if (response.script) { eval(response.script); }
+
+
+					$(document).trigger("netliva:ajaxSubmit:success", [response, statusText, xhr, $form]);
+					$form.trigger("netliva:ajaxSubmit:success", [response, statusText, xhr, $form]);
+
+				},
+				error: function (xhr, status, statusText, $form) {
+					$form.find('button').prop("disabled", 0);
+
+					$(document).trigger("netliva:ajaxSubmit:error", [status, xhr.status, statusText, xhr.responseText, $form, xhr]); // event, statusType, statusCode, statusText, response, $formElement, xhr
+					$form.trigger("netliva:ajaxSubmit:error", [status, xhr.status, statusText, xhr.responseText, $form, xhr]); // event, statusType, statusCode,  statusText, response, $formElement, xhr
+
+				},
 				type: $(this).attr("method")
 			}).removeClass("be_ajax_form").addClass("ajax_form");
 		});
 	};
 
-	window.ajaxFormSuccess = function (response, statusText, xhr, $form) {
-		$(".form-control").removeClass("is-invalid is-valid");
-		$form.find(".invalid-feedback").remove();
-		$form.find('button').prop("disabled", 0);
-		if (response.situ === 'success')
-		{
-
-			$form.before(
-				'<div class="alert alert-success" role="alert">'+
-				'<h4 class="alert-heading">'+(response.message !== undefined && response.message.title !== undefined?response.message.title:'İşlem Tamam')+'</h4>'+
-				'<p>'+(response.message !== undefined && response.message.content !== undefined ? response.message.content:'İşleminiz başarıyla gerçekleştirildi')+'</p>'+
-				'</div>'
-			);
-			if(response.removeForm === false)
-			{
-				$(".alert").slideUp(1000);
-			}
-			else
-			{
-				$form.remove();
-			}
-
-			if(response.hideModal)
-			{
-				if (typeof response.hideModal === 'boolean')
-					$(".modal").modal("hide");
-				else if (typeof response.hideModal === 'string')
-					$(response.hideModal).modal("hide");
-			}
-
-			if (response.alert !== undefined && typeof window.success == "function" )
-			{
-				window.success(response.alert);
-			}
-		}
-		else
-		{
-			if (response.alert !== undefined && typeof window.error == "function" )
-			{
-				window.error(response.alert);
-			}
-
-			$form.find(".form-control").each(function () {
-				$(this).addClass("is-valid");
-			});
-			$.each(response.errors, function(name, err) {
-				let errorsText = '<ul class="invalid-feedback list-unstyled">';
-				$.each(err, function(n, errorText) {
-					errorsText += "<li>"+errorText+"</li>";
-				});
-				errorsText += "</ul>";
-				$("#"+name)
-					.removeClass("is-valid")
-					.addClass("is-invalid")
-					.after(errorsText);
-			});
-
-		}
-		if (response.refresh) window.location.reload();
-		else if (response.url) { window.location.href = response.url; }
-		else if (response.script) { eval(response.script); }
-	};
 
 })(jQuery, window);
 
